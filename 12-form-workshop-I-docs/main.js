e2a8a2dc1b485f5bdb4ed784352dbf78c6db7065
Ohio <worrawutpanya@gmail.com>
2023-12-01 23:38:31 +0700

    Edited JS: submit-alert and Deleted class valid-feedback in 12-workshop && finished: 11-workshop

--- 12-form-workshop-I-docs/main.js
+++ 12-form-workshop-I-docs/main.js
@@ -7,9 +7,24 @@ const formSubmit = () => {
   form.addEventListener(
     "submit",
     (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const inputFirstName = document.getElementById("first-name").value;
+      const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+
       if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
+        // event.preventDefault();
+        // event.stopPropagation();
+      } else {
+        // event.preventDefault();
+        // event.stopPropagation();
+        alertPlaceholder.innerHTML = [
+          `<div class="alert alert-success alert-dismissible" role="alert">
+             <div>Nice, ${inputFirstName} has submitted this form success!</div>
+             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>`,
+        ];
       }
 
       form.classList.add("was-validated");
@@ -19,45 +34,3 @@ const formSubmit = () => {
 };
 
 formSubmit();
-
-//===================== ALERT SECTION ============================
-const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement("div");
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    "</div>",
-  ].join("");
-
-  alertPlaceholder.append(wrapper);
-};
-
-const alertTrigger = document.getElementById("liveAlertBtn");
-if (alertTrigger) {
-  alertTrigger.addEventListener("click", () => {
-    const inputFirstName = document.getElementById("first-name").value;
-    const inputLastName = document.getElementById("last-name").value;
-    const inputEmail = document.getElementById("input-email").value;
-    const inputPass = document.getElementById("input-pass").value;
-
-    // Email Pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // Reduce Pattern for Bootstrap5
-    const emailRegex = /^[^\s@]+@[^\s@]+$/;
-
-    // Use test() method for check the email input matches the pattern.
-    const emailValid = emailRegex.test(inputEmail);
-
-    const allInput =
-      inputFirstName === "" && inputLastName === "" && inputPass === "";
-
-    if (!allInput && emailValid) {
-      appendAlert(
-        `Nice, ${inputFirstName} ${inputLastName} submitted this form success!`,
-        "success"
-      );
-    }
-  });
-}
