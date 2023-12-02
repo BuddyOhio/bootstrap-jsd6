f477567a67bb698096cb8cd521bd87f9c9223e51
Ohio <worrawutpanya@gmail.com>
2023-12-02 18:25:32 +0700

    Edited JS: form.checkValidity() in 12-workshop

--- 12-form-workshop-I-docs/main.js
+++ 12-form-workshop-I-docs/main.js
@@ -13,12 +13,21 @@ const formSubmit = () => {
       const inputFirstName = document.getElementById("first-name").value;
       const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 
-      if (!form.checkValidity()) {
-        // event.preventDefault();
-        // event.stopPropagation();
-      } else {
-        // event.preventDefault();
-        // event.stopPropagation();
+      // if (!form.checkValidity()) {
+      //   // event.preventDefault();
+      //   // event.stopPropagation();
+      // } else {
+      //   // event.preventDefault();
+      //   // event.stopPropagation();
+      //   alertPlaceholder.innerHTML = [
+      //     `<div class="alert alert-success alert-dismissible" role="alert">
+      //        <div>Nice, ${inputFirstName} has submitted this form success!</div>
+      //        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      //     </div>`,
+      //   ];
+      // }
+
+      if (form.checkValidity()) {
         alertPlaceholder.innerHTML = [
           `<div class="alert alert-success alert-dismissible" role="alert">
              <div>Nice, ${inputFirstName} has submitted this form success!</div>
